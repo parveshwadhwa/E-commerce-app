fa5fa1adce2b02ae1320f4d1e9f71774560fd7c8
princecoder51002 <wadhwacorporations51002@gmail.com>
2021-08-20 19:16:40 +0530

    Learning Observable Pattern and 3 types of Request which we can from our database using firestore or any other

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -21,11 +21,28 @@ class ShopPage extends React.Component {
     {
         const {updateCollections} = this.props;
         const collectionRef = firestore.collection('collections');
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+
+        /***********We Get The data using query (Without Observable/Observer Pattern)*/ 
+
+        collectionRef.get().then(snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
             this.setState({loading: false});
         });
+
+        /*********We get The Data Using Fetch (An API Request) and in most of the projects we will use this  */
+
+        // fetch('https://firestore.googleapis.com/v1/projects/e-commerce-app-5727b/databases/(default)/documents/collections')
+        // .then(response => response.json())
+        // .then(collections => console.log(collections));
+
+        /************(We get the data using observable/observer pattern)************* */
+        
+        // this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+        //     const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        //     updateCollections(collectionsMap);
+        //     this.setState({loading: false});
+        // });
     }
     render()
     {
